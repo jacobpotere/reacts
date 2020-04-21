b633564c373593bef75851f14fe4c5a42154288f
Jacob Potere <jacob.potere@students.dominican.edu>
2020-04-20 23:31:32 -0700

    dynamic content and footer

---
+++ src/POPOSSpace.js
@@ -0,0 +1,17 @@
+// src/POPOSSPace.js
+
+import React from 'react'
+
+function POPOSSpace(props) {
+  const { name, image, address, hours } = props
+  return (
+    <div className="POPOSSpace">
+      <h1>{name}</h1>
+      <img src={`${process.env.PUBLIC_URL}images/${image}`} width="300" height="300" alt="Hello" />
+      <div>{address}</div>
+      <div>{hours}</div>
+    </div>
+  )
+}
+
+export default POPOSSpace
---
+++ src/Title.js
@@ -0,0 +1,18 @@
+// src/Title.js
+
+import './Title.css';
+import React from 'react'
+
+
+function Title() {
+  return (
+    <div className="Title">
+      <header>
+        <h1>SFPOPOS</h1>
+        <div className="Title-Subtitle">San Francisco Privately Owned Public Open Spaces</div>
+      </header>
+    </div>
+  )
+}
+
+export default Title
